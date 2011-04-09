303030bb1d2776cd5d19bfb0e3e1c79538838dfc
nashby <younash@gmail.com>
2011-04-10 00:58:58 +0300

    add 05 from the misc section

--- problems/misc/05.rb
+++ problems/misc/05.rb
@@ -1,1 +1,21 @@
-#TODO
+# 7.05 (**) English number words
+
+# On financial documents, like cheques, numbers must sometimes be written in full words. Example: 175 must be written as one-seven-five. Write a predicate full_words/1 to print (non-negative) integer numbers in full words.
+
+module Problems
+  module Misc
+    def self.full_words(number)
+      digits = {0 => "zero",
+                   1 => "one", 
+                   2 => "two",
+                   3 => "three",
+                   4 => "four",
+                   5 => "five",
+                   6 => "six",
+                   7 => "seven",
+                   8 => "eight",
+                   9 => "nine"}
+      number.to_s.split(//).map{|digit| digits[digit.to_i]}.join("-")
+    end
+  end
+end
--- test/misc_test/test_05.rb
+++ test/misc_test/test_05.rb
@@ -1,1 +1,11 @@
-#TODO
+require 'test_helper'
+require 'misc/05'
+
+class Problem05Test < Test::Unit::TestCase
+	context "a problem 5 " do
+    should "print (non-negative) integer numbers in full words" do
+      assert_equal "one-seven-five", Problems::Misc.full_words(175)
+      assert_equal "zero", Problems::Misc.full_words(0)
+    end
+  end
+end
